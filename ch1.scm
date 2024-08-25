b243e436e2bde2a5637905a47ace705cf500a347
fyshx <fyshx121120@gmail.com>
2024-08-25 12:27:37 +0800

    first commit

---
+++ ch1.scm
@@ -0,0 +1,23 @@
+(define (abs x)
+  ( cond ((> x 0) x)
+         ((< x 0) (- x))
+         ((= x 0) 0)        
+  )
+)
+
+(define (abss x )
+    (cond (( > x 0 )x )
+          (else (- x))
+    )
+)
+
+(define (absss x)
+    (if (< x 0) 
+        (- x) 
+        x
+    )
+)
+
+
+; (define (:- f x)) 不能定义出和Haskell中一样的-:的原因是, lisp中的函数调用都是前缀调用
+
